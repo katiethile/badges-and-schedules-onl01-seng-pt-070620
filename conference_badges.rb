031829e3b62ddbebabff4ca263028ed97d2e01df
Katie Le <lethikatie@gmail.com>
2020-07-10 18:36:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms (speakers)
   room = 0
   speakers.each do |speaker|
-  speakers[room] = "Hello,#{speaker}! You'll be assigned to room #{room + !}!"
+  speakers[room] = "Hello,#{speaker}! You'll be assigned to room #{room + }!"
   room += 1
   end
 end
