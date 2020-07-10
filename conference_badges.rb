61ec14b26fabfe376ca6f2b887808febac657281
Katie Le <lethikatie@gmail.com>
2020-07-10 18:28:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,14 @@ def batch_badge_creator (attendees)
 end
 
 
+def assign_rooms (speakers)
+  room = 0
+  speakers.each do |speaker|
+  speakers[room] = "Hello,#{speaker}! You'll be assigned to room #{room + 1!}!"
+  room += 1
+  end
+end
+
+def printer 
+
+end
