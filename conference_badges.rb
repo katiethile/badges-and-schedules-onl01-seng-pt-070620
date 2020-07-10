ea9cb5a38d0c50beee722f9acdc97f7531826593
Katie Le <lethikatie@gmail.com>
2020-07-10 18:28:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,20 +11,3 @@ def batch_badge_creator (attendees)
 end
 
 
-Failures:
-
-  1) conference_badges #assign_rooms should return a list of welcome messages and room assignments
-     Failure/Error: expect(assign_rooms(attendees)).to eq(room_assignments)
-
-     NoMethodError:
-       undefined method `assign_rooms' for #<RSpec::ExampleGroups::ConferenceBadges::AssignRooms:0x0000000001ae86d8>
-     # ./spec/conference_badges_spec.rb:82:in `block (3 levels) in <top (required)>'
-
-  2) conference_badges #assign_rooms should not hard-code the response
-     Failure/Error: expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
-
-     NoMethodError:
-       undefined method `assign_rooms' for #<RSpec::ExampleGroups::ConferenceBadges::AssignRooms:0x000000000179d140>
-     # ./spec/conference_badges_spec.rb:85:in `block (3 levels) in <top (required)>'
-
-  3) conference_badges #printer should puts the list of badges and room_ass
