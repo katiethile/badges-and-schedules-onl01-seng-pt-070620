2d160c2d4cf011c2fcac387e28c403ed163966e1
Katie Le <lethikatie@gmail.com>
2020-07-10 19:44:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,8 +21,9 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |attendee|
-  
+    puts attendee
   end
   assign_rooms(attendees).each do |speaker|
+    puts speaker
   end
 end 
