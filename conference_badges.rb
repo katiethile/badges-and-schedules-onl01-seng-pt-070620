1e93050fd36dd75af0c79798980ebcebcc2127da
Katie Le <lethikatie@gmail.com>
2020-07-10 18:17:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  i = 0
+  attendees.each do |attendee|
+  attendees[i] = badge_maker(attendee)
+  i += 1
+  end
+end
+
+
+def assign_rooms (speakers)
+  room = 0
+  speakers.each do |speaker|
+  speakers[room] = "Hello,#{speaker}! You'll be assigned to room #{room + 1!}!"
+  room += 1
+  end
+end
+
+def printer 
+
+end
